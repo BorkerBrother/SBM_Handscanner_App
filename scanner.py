9681fa3454884674321f98bc78f2d784e58d097f
Kai Borker <borkerkai@yahoo.de>
2023-11-17 13:14:04 +0100

    Get GTIN & EAN with Scanner

--- scanner.py
+++ scanner.py
@@ -2,14 +2,15 @@ import PySimpleGUI as sg
 
 sg.theme("Dark Blue 3")
 
-text_elem = sg.Text(size=(18, 1), key='-GTIN-', enable_events=True)
+character_array = []
+text_elem = sg.Text(size=(50, 1), key='-GTIN-', enable_events=True, text_color="white", font=20)
 
 layout =    [[sg.Text("Scan your EAN/GTIN Code")],
-            [sg.Text("EAN/GTIN:"), [text_elem] ],
-            [sg.Button("Close", size=(15,1))]]
+            [sg.Text("EAN/GTIN:", font=20), (text_elem)],
+            [sg.Button("Close", size=(15,1)), sg.Button("New", size=(15,1)) ]]
 
 # Create the window
-window = sg.Window("Handscanner", layout, size=(400,200), return_keyboard_events=True, use_default_focus=False)
+window = sg.Window("Handscanner", layout, size=(300,100), return_keyboard_events=True, use_default_focus=False)
 
 # Create an event loop
 while True:
@@ -21,9 +22,18 @@ while True:
     elif event == sg.WIN_CLOSED:
         break
 
+    if event == "New" or event == "BackSpace:8":
+        # Clear the character array
+        character_array = []
+        text_elem.update("")
 
-    text_elem.update(event)
+    if event != "New":
+        # Update the text element and append the character to the array
+        character = event
+        text_elem.update(''.join(character_array + [character]))
+        character_array.append(character)
 
+    print(event)
 window.close()
 
 
